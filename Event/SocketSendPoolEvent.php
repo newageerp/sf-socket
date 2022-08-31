7443fe8ead998f2517c5644f12a81e8836f1a72b
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-08-31 06:37:35 +0000

    updates

--- Event/SocketSendPoolEvent.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Newageerp\SfSocket\Event;
-
-use Symfony\Contracts\EventDispatcher\Event;
-
-class SocketSendPoolEvent extends Event
-{
-    public const NAME = 'sfsocket.sendpool';
-}
